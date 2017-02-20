5302b7f83d62945477aac19ab908c4d08a665d17
tylorte <mm@tqy-mbp.local>
2017-02-20 10:58:58 +0800

    提交第一次版本
    包括Hello程序

---
+++ GitIOSDemo/GitIOSDemo/HelloFile.swift
@@ -0,0 +1,17 @@
+//
+//  HelloFile.swift
+//  GitIOSDemo
+//
+//  Created by 谭其勇 on 2017/2/20.
+//  Copyright © 2017年 谭其勇. All rights reserved.
+//
+
+import UIKit
+
+class HelloFile: NSObject {
+    
+    func sayHello(_ name:String?) -> String {
+        return "Hello,my dear \(name)"
+    }
+
+}
---
+++ GitIOSDemo/GitIOSDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GitIOSDemo
+//
+//  Created by 谭其勇 on 2017/2/20.
+//  Copyright © 2017年 谭其勇. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
